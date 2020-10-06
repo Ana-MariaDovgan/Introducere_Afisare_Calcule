f2415a4459b7ae4f70d8b1f0c91a3354990a1d0e
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-06 15:19:32 +0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,3 +1,10 @@
+Un brăduţ este împodobit cu globuleţe albe, roşii şi albastre. Numărul globuleţelor
+albe se citeşte de la tastatură. Câte globuleţe are brăduţul, ştiind că numărul de
+globuleţe roşii este cu 3 mai mare decât numărul de globuleţe albe, iar globuleţele
+albastre sunt cu 2 mai puţine decât totalul celor albe şi roşii. Exemplu: Date de intrare:
+12 Date de ieşire: 52.
+
+
 na=int(input("dati numarul de globuri albe"))
 nr= na + 3
 nb= (na + nr) - 2
