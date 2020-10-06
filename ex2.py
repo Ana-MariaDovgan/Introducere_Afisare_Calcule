6b2d8404d0d01c68dd7971a4f3c69452fcb24fee
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-06 15:17:58 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,3 +1,7 @@
+Într-o tabără numărul de băieţi este cu 10 mai mare decât cel al fetelor. Dacă se citeşte
+de la tastatură numărul de fete, să se spună câţi elevi sunt în tabără. Exemplu: date de
+intrare: 50 date de ieşire: 110
+    
 nf=int(input("dati numarul de fete"))
 nb= nf + 10
 nt= nb + nf
