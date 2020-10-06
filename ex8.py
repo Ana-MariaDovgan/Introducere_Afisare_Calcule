4bc2b934cad320c2f39b26988e61a98532dc437f
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-06 15:22:05 +0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,3 +1,7 @@
+Se introduc de la tastatură trei cifre. Afişaţi pe aceeaşi linie 5 numere formate cu
+aceste cifre luate o singură dată. Exemplu : date de intrare : 3 4 2 Date de ieşire : 324
+342 243 234 432.
+
 a=int(input("dati a= "))
 b=int(input("dati b= "))
 c=int(input("dati c= "))
