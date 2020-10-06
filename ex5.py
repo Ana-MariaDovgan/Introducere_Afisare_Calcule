43404dc89d2c42e1b6043a23425d897f2135921d
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-06 15:20:01 +0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,2 +1,7 @@
+Ion şi Vasile joacă următorul joc: Ion spune un număr iar Vasile trebuie să găsească
+cinci numere consecutive, crescătoare, numărul din mijloc fiind cel ales de Ion.
+Exemplu : Ion spune 10, Vasile spune 8 9 10 11 12. Ajutaţi-l pe Vasile să găsească
+răspunsul mai repede
+.
 nr=int(input("dati un numar"))
 print(nr-2,nr-1,nr+1,nr+2,sep = "_")
