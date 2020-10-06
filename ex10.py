c0f1faef5a408ee0ea4248c8ab261b411081f239
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-06 15:23:12 +0300

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,3 +1,6 @@
+Afişaţi tabla înmulţirii cu numărul n. Exemplu: pentru n=5, se va afişa pe verticală
+1x5=5 2x5=10 3x5=15 4x5=20 5x5=25 6x5=30 7x5=35 8x5=40 9x5=45 10x5=50.
+
 n=int(input("dati un nummar"))
 print(n,"*" ,1, "=",n*1)
 print(n,"*" ,2, "=",n*2)
