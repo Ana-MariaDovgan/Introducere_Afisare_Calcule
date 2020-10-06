cbd928a1175fe0030513c4f06dc5ff2e54b426d8
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-06 15:23:42 +0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,4 @@
+ni=int(input("dati numarul de iepuri la inceput de luna: "))
+nm=int(input("dati numarul de iepuri morti: "))
+nn=int(input("dati numarul de iepuri nascuti: "))
+print("Numarul de iepuri la sfarsit de luna este : ",ni- nm + nn)
